d2d072b36b30e9f82500096051da967c9ec15572
Bastian Gutierrez <bas.gutierrez@gmail.com>
2023-08-22 20:06:49 -0400

    Create card of pokemon

---
+++ src/Components/PokemonCard.jsx
@@ -0,0 +1,12 @@
+export const PokemonCard = ({name, type, imgFront}) => {
+  return (
+    <div className="w-48 h-80 p-5
+    border-solid border rounded-lg
+    flex flex-col justify-around items-center 
+    bg-amber-200">
+      <h2 className="text-xl	">{name.toUpperCase()}</h2>
+      <h3>{type.toUpperCase()}</h3>
+      <img src={imgFront} alt="imagen de pokemon" />
+    </div>
+  );
+};
