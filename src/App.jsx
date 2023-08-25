1c88a156a2dc1bf390962623de2109f46799d580
Bastian Gutierrez <bas.gutierrez@gmail.com>
2023-08-24 20:51:14 -0400

    Cambios en formato

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,41 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import { MiApi } from './Components/MiApi'
-import {cantPokemonGeneracion} from './logic/const'
-import { Header } from './Components/Header'
-import { Navbar } from './Components/Navbar'
+import { useEffect, useState } from "react";
+import "./App.css";
+import { MiApi } from "./Components/MiApi";
+import { cantPokemonGeneracion } from "./logic/const";
+import { Header } from "./Components/Header";
+import { Navbar } from "./Components/Navbar";
+import { Searcher } from "./Components/Searcher";
+
 function App() {
-const [listPokemon, setListPokemon]=useState([1,2,3,4,5,6,7,8,9,10,11,12])
-const [generation, setGeneration]=useState(2)
-const [RenderPage,setRenderPage]=useState(1)
-useEffect(()=>{
-const listRender=[1,2,3,4,5,6,7,8,9,10,11,12]
-const arrayList=listRender.map((idPoke)=> idPoke+12*RenderPage-12)
-setListPokemon(arrayList)
-}
-,[RenderPage,generation])
-const handleChange=(e)=>{
-  if(e.target.value>0){
-    setRenderPage(e.target.value)
-  }
-}
+  const [listPokemon, setListPokemon] = useState([
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
+  ]);
+  const [generation, setGeneration] = useState(2);
+  const [RenderPage, setRenderPage] = useState(1);
+  useEffect(() => {
+    const listRender = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+    const arrayList = listRender.map((idPoke) => idPoke + 12 * RenderPage - 12);
+    setListPokemon(arrayList);
+  }, [RenderPage, generation]);
+  const handleChange = (e) => {
+    if (e.target.value > 0) {
+      setRenderPage(e.target.value);
+    }
+  };
   return (
     <>
-    <Header />
-    <div className='flex justify-center gap-5'>
-    <Navbar />
-    <div className='w-5/6 grid grid-cols-3 gap-2 place-items-center'>
-    {listPokemon.map((idPoke)=> 
-      <MiApi key={idPoke} idPoke={idPoke}/>
-    )}
-    <input type="number" min="1" onChange={handleChange} />
-    </div>
-    </div>
+      <Header />
+      <div className="flex justify-evenly gap-5">
+        <Navbar />
+        <div className="w-[1000px] h-[710px] grid grid-cols-1 gap-2 place-items-center">
+          {listPokemon.map((idPoke) => (
+            <MiApi key={idPoke} idPoke={idPoke} />
+          ))}
+          <input type="number" min="1" onChange={handleChange} />
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,10 +1,11 @@
 import { colorFromType } from "../logic/const";
 import { BadgeType } from "./BadgeType";
+
 export const Navbar = () => {
   const types = Object.keys(colorFromType);
 
   return (
-    <nav className="w-1/3 flex flex-col gap-3 items-center">
+    <nav className="w-80 flex flex-col gap-3 items-center">
       <h2 className="w-full">Filtrar por Tipo</h2>
       <div className="flex flex-wrap gap-5 w-20">
         {types.map((type) => (
