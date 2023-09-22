aaf87445e6cc6632cc86d4dbd10b60e102a28a63
Bastian Gutiérrez <bas.gutierrez@gmail.com>
2023-09-22 11:08:50 -0300

    rendondeo en stats

--- src/Components/BarStats.jsx
+++ src/Components/BarStats.jsx
@@ -2,11 +2,11 @@ export const BarStats=({name,value,color})=>{
 const BACKGROUND_FILL=`bg-${color}`
 const BACKGROUND_EMPTY="bg-gray-600"
 
-const CLASSNAME_1=`w-4 h-4 border border-black ${value>0?BACKGROUND_FILL:BACKGROUND_EMPTY}`
-const CLASSNAME_2=`w-4 h-4 border border-black ${value>30?BACKGROUND_FILL:BACKGROUND_EMPTY}`
-const CLASSNAME_3=`w-4 h-4 border border-black ${value>60?BACKGROUND_FILL:BACKGROUND_EMPTY}`
-const CLASSNAME_4=`w-4 h-4 border border-black ${value>90?BACKGROUND_FILL:BACKGROUND_EMPTY}`
-const CLASSNAME_5=`w-4 h-4 border border-black ${value>120?BACKGROUND_FILL:BACKGROUND_EMPTY}`
+const CLASSNAME_1=`w-4 h-4 border border-black rounded ${value>0?BACKGROUND_FILL:BACKGROUND_EMPTY}`
+const CLASSNAME_2=`w-4 h-4 border border-black rounded  ${value>30?BACKGROUND_FILL:BACKGROUND_EMPTY}`
+const CLASSNAME_3=`w-4 h-4 border border-black rounded  ${value>60?BACKGROUND_FILL:BACKGROUND_EMPTY}`
+const CLASSNAME_4=`w-4 h-4 border border-black rounded  ${value>90?BACKGROUND_FILL:BACKGROUND_EMPTY}`
+const CLASSNAME_5=`w-4 h-4 border border-black rounded  ${value>120?BACKGROUND_FILL:BACKGROUND_EMPTY}`
 
     return(
     <div className="w-1/2 flex gap-2 items-center justify-between text-white">
